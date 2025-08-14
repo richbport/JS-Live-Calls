a81e059bdc7e03de4ca5bea3418ba8dc8bad6d13
Rich <richbport@yahoo.com>
2025-08-14 16:38:20 -0400

    .filter

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 // let ballPrices = [20, 25, 5, 15, 30]
 
 // let totalPrice = ballPrices.reduce((totalPrice) => {
-//       return 'golden ' + ball
-// })
+    //       return 'golden ' + ball
+    // })
+    
+    // let ballPrices = [20, 25, 5, 15, 30]
+    
+    // let totalPrice = ballPrices.reduce((total, price) => total + price, 0);
+    
+    // console.log(totalPrice)
+    
+    // total = 0, first price = 20 === 20
+    // total = 20, second price = 25 === 45
+    // total = 45, third price = 5 === 50
+    // total = 50, third price = 15 === 65
+    // total = 65, third price = 30 === 95
 
-let ballPrices = [20, 25, 5, 15, 30]
+    let basket = ['soccer', 'basketball', 'tennis', 'football', 'volleyball',]
+    
+    console.log(basket.length)
 
-let totalPrice = ballPrices.reduce((total, price) => {
-    return total + price
-}, 0,)
-
-console.log(totalPrice)
-
-// total = 0, first price = 20 === 20
-// total = 20, second price = 25 === 45
-// total = 45, third price = 5 === 50
-// total = 50, third price = 15 === 65
-// total = 65, third price = 30 === 95
+    let longNameBalls = basket.filter((ball) => {
+        return ball.length
+    })
